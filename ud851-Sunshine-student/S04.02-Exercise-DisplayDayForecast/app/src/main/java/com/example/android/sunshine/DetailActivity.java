f18f9d14449cc839eb49b059ed2c31666ab830a9
fireflyfif <fif.iva@gmail.com>
2017-12-26 18:53:04 +0100

    complete S04.02-Exercise-DisplayDayForecast

--- ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,32 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+    private TextView weatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        weatherData = (TextView) findViewById(R.id.weather_data_displayed);
+
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+        Intent intentThatWasPassed = getIntent();
+        if (intentThatWasPassed != null) {
+            if (intentThatWasPassed.hasExtra(Intent.EXTRA_PACKAGE_NAME)) {
+                mForecast = intentThatWasPassed.getStringExtra(Intent.EXTRA_PACKAGE_NAME);
+                weatherData.setText(mForecast);
+            }
+
+        }
     }
 }
