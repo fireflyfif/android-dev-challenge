685ab6a30d157d1b7aba84124560b5e72cb3cebe
fireflyfif <fif.iva@gmail.com>
2018-01-02 14:02:27 +0100

    complete T06.02-Exercise-MakeAPreferenceFragment

---
+++ ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,16 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by iva on 10/24/2017.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
