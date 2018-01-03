b054604e8f8fd8559694e37b1c0bf2cde435fdfc
fireflyfif <fif.iva@gmail.com>
2018-01-03 16:22:22 +0100

    complete T07.04-Exercise-UpdateTheAdapter

--- ud851-Exercises-student/Lesson07-Waitlist/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ ud851-Exercises-student/Lesson07-Waitlist/T07.04-Exercise-UpdateTheAdapter/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -14,18 +14,18 @@ import com.example.android.waitlist.data.WaitlistContract;
 public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
 
     private Context mContext;
-    // TODO (1) Replace the mCount with a Cursor field called mCursor
-    private int mCount;
+    // COMPLETED (1) Replace the mCount with a Cursor field called mCursor
+    private Cursor mCursor;
 
     /**
      * Constructor using the context and the db cursor
      * @param context the calling context/activity
      */
-    // TODO (2) Modify the constructor to accept a cursor rather than an integer
-    public GuestListAdapter(Context context, int count) {
+    // COMPLETED (2) Modify the constructor to accept a cursor rather than an integer
+    public GuestListAdapter(Context context, Cursor cursor) {
         this.mContext = context;
-        // TODO (3) Set the local mCursor to be equal to cursor
-        mCount = count;
+        // COMPLETED (3) Set the local mCursor to be equal to cursor
+        mCursor = cursor;
     }
 
     @Override
@@ -38,21 +38,27 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     @Override
     public void onBindViewHolder(GuestViewHolder holder, int position) {
-        // TODO (5) Move the cursor to the passed in position, return if moveToPosition returns false
+        // COMPLETED (5) Move the cursor to the passed in position, return if moveToPosition returns false
+        if (!mCursor.move(position)) {
+            return;
+        }
+        // COMPLETED (6) Call getString on the cursor to get the guest's name
+        String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
 
-        // TODO (6) Call getString on the cursor to get the guest's name
+        // COMPLETED (7) Call getInt on the cursor to get the party size
+        int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
 
-        // TODO (7) Call getInt on the cursor to get the party size
+        // COMPLETED (8) Set the holder's nameTextView text to the guest's name
+        holder.nameTextView.setText(name);
 
-        // TODO (8) Set the holder's nameTextView text to the guest's name
-
-        // TODO (9) Set the holder's partySizeTextView text to the party size
+        // COMPLETED (9) Set the holder's partySizeTextView text to the party size
+        holder.partySizeTextView.setText(partySize);
     }
 
     @Override
     public int getItemCount() {
-        // TODO (4) Update the getItemCount to return the getCount of mCursor
-        return mCount;
+        // COMPLETED (4) Update the getItemCount to return the getCount of mCursor
+        return mCursor.getCount();
     }
 
 
@@ -75,9 +81,8 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
          */
         public GuestViewHolder(View itemView) {
             super(itemView);
-            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
-            partySizeTextView = (TextView) itemView.findViewById(R.id.party_size_text_view);
+            nameTextView = itemView.findViewById(R.id.name_text_view);
+            partySizeTextView = itemView.findViewById(R.id.party_size_text_view);
         }
-
     }
 }
