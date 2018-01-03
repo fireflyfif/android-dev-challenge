2542ca6e48e9aca95ccdc28114bcca66cde8c5d6
fireflyfif <fif.iva@gmail.com>
2018-01-03 16:28:16 +0100

    complete T07.05-Exercise-AddGuests

--- ud851-Exercises-student/Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ ud851-Exercises-student/Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -57,13 +57,23 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         return mCursor.getCount();
     }
 
-    // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+    // COMPLETED (15) Create a new function called swapCursor that takes the new cursor and returns void
+    public void swapCursor(Cursor newCursor) {
+        // COMPLETED (16) Inside, check if the current cursor is not null, and close it if so
+        // Always close the previous mCursor first
+        if (mCursor != null) {
+            mCursor.close();
+        }
 
-    // TODO (16) Inside, check if the current cursor is not null, and close it if so
+        // COMPLETED (17) Update the local mCursor to be equal to  newCursor
+        mCursor = newCursor;
 
-    // TODO (17) Update the local mCursor to be equal to  newCursor
-
-    // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        // COMPLETED (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        if (newCursor != null) {
+            // Force the RecyclerView to refresh
+            this.notifyDataSetChanged();
+        }
+    }
 
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
@@ -84,9 +94,8 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
          */
         public GuestViewHolder(View itemView) {
             super(itemView);
-            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
-            partySizeTextView = (TextView) itemView.findViewById(R.id.party_size_text_view);
+            nameTextView = itemView.findViewById(R.id.name_text_view);
+            partySizeTextView = itemView.findViewById(R.id.party_size_text_view);
         }
-
     }
 }
